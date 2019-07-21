a69018b1e65d45d1f4692cdc3ec3aac5f4bb414e
sughracse <sughra788@gmail.com>
2019-07-21 14:34:17 +0530

    first commit

---
+++ src/Component/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Stylecss/headerStyle.css"
+
+const Header =()=>{
+    return(
+        <div className="header">
+            <p className="paragraph">Header</p>
+        </div>
+    );
+};
+export default Header;
